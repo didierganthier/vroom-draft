cb8585e014a4a2a1b4b9f19c0ab4ac519b42060d
didierganthier <didierganthierperan@gmail.com>
2021-06-29 14:35:47 -0400

    forgot_password.dart route defined

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vroom/src/pages/forgot_password.dart';
 import 'package:vroom/src/pages/login_page.dart';
 
 //Pages
@@ -6,5 +7,6 @@ import 'package:vroom/src/pages/welcome_screen.dart';
 
 final routes = <String, WidgetBuilder>{
   'welcome' : (BuildContext context) => WelcomePage(),
-  'login' : (BuildContext context) => LoginPage()
+  'login' : (BuildContext context) => LoginPage(),
+  'forgot-password' : (BuildContext context) => ForgotPassword(),
 };
