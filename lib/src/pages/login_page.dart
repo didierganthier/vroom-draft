53c790038cee8a34fa54d50de1ac0e960a3f6c71
didierganthier <didierganthierperan@gmail.com>
2021-06-29 14:33:50 -0400

    login_page.dart designed

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,33 +1,179 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:vroom/src/colors/colors.dart';
+import 'package:vroom/src/widgets/back_button.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
     SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle.dark.copyWith(
+      SystemUiOverlayStyle.light.copyWith(
         statusBarColor: Colors.black,
       )
     );
 
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Image(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: true,
+        leading: backButton(context, Colors.white),
+      ),
+      extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image(
+                  width: double.infinity,
+                  height: 350,
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=653&q=80'
+                  ),
+                ),
+              ],
+            ),
+            Transform.translate(
+              offset: Offset(0.0, -20.0),
+              child: Container(
                 width: double.infinity,
-                height: 350,
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                    'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=653&q=80'
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Center(
+                    child: Column(
+                      children: [
+                         Text("Welcome Back", style: TextStyle(
+                           color: Theme.of(context).primaryColor,
+                           fontWeight: FontWeight.bold,
+                           fontSize: 30.0,
+                         ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 15.0),
+                          child: Text("Login to your account", style: TextStyle(
+                            color: gris,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 15.0,
+                           ),
+                          ),
+                        ),
+                        _emailInput(),
+                        _passwordInput(),
+                        _buttonLogin(context),
+                        Container(
+                          margin: EdgeInsets.only(top: 30.0),
+                          child: Text('Forgot your password?',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 30.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Don't have an account?",
+                                style: TextStyle(
+                                  color: gris,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 15.0,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                                child: Text("Sign Up",
+                                  style: TextStyle(
+                                    color: Theme.of(context).accentColor,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 15.0,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ],
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget _emailInput(){
+  return Container(
+    margin: EdgeInsets.only(top: 40.0),
+    padding: EdgeInsets.only(left: 20.0),
+    decoration: BoxDecoration(
+      color: Color.fromRGBO(142, 142, 147, 1.2),
+      borderRadius: BorderRadius.circular(30.0),
+    ),
+    child: TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _passwordInput(){
+  return Container(
+    margin: EdgeInsets.only(top: 15.0),
+    padding: EdgeInsets.only(left: 20.0),
+    decoration: BoxDecoration(
+      color: Color.fromRGBO(142, 142, 147, 1.2),
+      borderRadius: BorderRadius.circular(30.0),
+    ),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget _buttonLogin(BuildContext context){
+  return Container(
+    margin: EdgeInsets.only(top: 15.0),
+    width: MediaQuery.of(context).size.width - 35,
+    height: 45.0,
+    child: RaisedButton(
+      onPressed: () {
+        Navigator.pushNamed(context, 'login');
+      },
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0)
+      ),
+      color: Theme.of(context).accentColor,
+      child: Text('Log In', style: TextStyle(
+        color: Colors.white,
+        fontSize: 15.0,
+      ),
+      ),
+    ),
+  );
 }
