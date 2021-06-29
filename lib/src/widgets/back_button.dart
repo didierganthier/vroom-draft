6c1ccf77a18baa006559503f9b82d6c58178c9ae
didierganthier <didierganthierperan@gmail.com>
2021-06-29 11:13:56 -0400

    back_button.dart reusable widget designed

---
+++ lib/src/widgets/back_button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Widget backButton(BuildContext context, Color color){
+  return IconButton(
+    icon: Icon(
+      Icons.arrow_back_ios,
+      color: color,
+      size: 28.0,
+    ),
+    onPressed: () {
+      Navigator.pop(context);
+    },
+  );
+}
