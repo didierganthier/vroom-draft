45d11e2f6469fed5b0010ffd9c69593372e8ce91
didierganthier <didierganthierperan@gmail.com>
2021-06-30 11:46:31 -0400

    route to tabs_page.dart defined in routes.dart

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+
+//Pages
 import 'package:vroom/src/pages/forgot_password.dart';
 import 'package:vroom/src/pages/login_page.dart';
 import 'package:vroom/src/pages/sign_up_page.dart';
-
-//Pages
 import 'package:vroom/src/pages/welcome_screen.dart';
+import 'package:vroom/src/tabs/tabs_page.dart';
 
 final routes = <String, WidgetBuilder>{
   'welcome' : (BuildContext context) => WelcomePage(),
   'login' : (BuildContext context) => LoginPage(),
   'forgot-password' : (BuildContext context) => ForgotPassword(),
   'sign-up' : (BuildContext context) => SignUpPage(),
+  'tabs' : (BuildContext context) => TabsPage(),
 };
---
+++ lib/src/tabs/tabs_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TabsPage extends StatefulWidget {
+  @override
+  _TabsPageState createState() => _TabsPageState();
+}
+
+class _TabsPageState extends State<TabsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Hello'),
+    );
+  }
+}
