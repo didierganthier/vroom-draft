ee7b18fd616d81a81005b8663f6189bd7d30f034
Woodens <woodgregory15@gmail.com>
2021-07-15 13:32:52 -0400

    stuff

--- lib/src/features/presentation/filter_page/View/customsWidgets/list_tile_checked.dart
+++ lib/src/features/presentation/filter_page/View/customsWidgets/list_tile_checked.dart
@@ -3,6 +3,11 @@ import 'package:vroom/src/colors/colors.dart';
 import 'package:vroom/src/features/presentation/widgets/header_text.dart';
 
 class ListTileCheck extends StatefulWidget {
+  String texto;
+  bool isActive;
+  Function function;
+
+  ListTileCheck({Key key, this.texto, this.isActive, this.function}) : super(key: key);
   @override
   _ListTileCheckState createState() => _ListTileCheckState();
 }
@@ -12,31 +17,30 @@ class _ListTileCheckState extends State<ListTileCheck> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        listTiles(context: context, texto: 'Top Rated')
+        listTiles(
+            context: context, texto: widget.texto, isActive: widget.isActive, function: widget.function)
       ],
     );
   }
 }
 
-
-Widget listTiles({BuildContext context, texto: String, isActive:bool, Function function }) {
+Widget listTiles(
+    {BuildContext context, texto: String, isActive: bool, Function function}) {
   return Container(
     decoration: BoxDecoration(
-      border: Border(
-        bottom: BorderSide(
-          color: Theme.of(context).dividerColor
-        )
-      )
-    ),
+        border:
+            Border(bottom: BorderSide(color: Theme.of(context).dividerColor))),
     child: ListTile(
       onTap: function,
       title: headerText(
-        texto: texto,
-        color: orange,
-        fontWeight: FontWeight.w300,
-        fontsize: 17
-      ),
-      trailing: Icon(Icons.check, color: orange,),
+          texto: texto,
+          color: isActive ? orange : Colors.black,
+          fontWeight: FontWeight.w300,
+          fontsize: 17),
+      trailing: isActive ? Icon(
+        Icons.check,
+        color: isActive ? orange : gris,
+      ) : Text(''),
     ),
   );
 }
--- lib/src/features/presentation/filter_page/View/filter_page.dart
+++ lib/src/features/presentation/filter_page/View/filter_page.dart
@@ -2,14 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:vroom/src/colors/colors.dart';
 import 'package:vroom/src/features/presentation/filter_page/View/customsWidgets/cuisines_filter.dart';
 import 'package:vroom/src/features/presentation/filter_page/View/customsWidgets/list_tile_checked.dart';
+import 'package:vroom/src/features/presentation/filter_page/View/customsWidgets/price_filter.dart';
 import 'package:vroom/src/features/presentation/widgets/header_text.dart';
 
 class FilterPage extends StatefulWidget {
+  FilterPage({Key key}) : super(key: key);
   @override
   _FilterPageState createState() => _FilterPageState();
 }
 
 class _FilterPageState extends State<FilterPage> {
+  //SORT BY BOOLEANS
+  bool topRated = false;
+  bool nearMe = false;
+  bool costHighToLow = false;
+  bool costLowToHigh = false;
+  bool mostPopular = false;
+
+  // FILTER BOOLEANS
+  bool openNow = false;
+  bool creditCards = false;
+  bool alcoholServed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,22 +32,22 @@ class _FilterPageState extends State<FilterPage> {
         slivers: [
           SliverList(
               delegate: SliverChildListDelegate([
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    margin: EdgeInsets.only(top:  15.0, bottom: 15.0, left: 15.0),
-                    child: headerText(
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(top: 15.0, bottom: 15.0, left: 15.0),
+                  child: headerText(
                       texto: 'CUISINES',
                       color: gris,
                       fontWeight: FontWeight.w600,
                       fontsize: 17.0
-                    ),
                   ),
-                  Container(
-                      padding: EdgeInsets.symmetric(horizontal: 15.0),
-                      child: CuisinesFilter()),
+                ),
+                Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: CuisinesFilter()),
                 Container(
                   alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.only(top:  15.0, bottom: 15.0, left: 15.0),
+                  margin: EdgeInsets.only(top: 15.0, bottom: 5.0, left: 15.0),
                   child: headerText(
                       texto: 'SORT BY',
                       color: gris,
@@ -43,8 +57,94 @@ class _FilterPageState extends State<FilterPage> {
                 ),
                 Container(
                     padding: EdgeInsets.symmetric(horizontal: 15.0),
-                    child: ListTileCheck()),
-                ]
+                    child: Column(
+                      children: [
+                        ListTileCheck(
+                          texto: 'Top Rated',
+                          isActive: topRated,
+                          function: () {
+                            setState(() => topRated = !topRated);
+                          },
+                        ),
+                        ListTileCheck(
+                          texto: 'Nearest Me',
+                          isActive: nearMe,
+                          function: () {
+                            setState(() => nearMe = !nearMe);
+                          },
+                        ),
+                        ListTileCheck(
+                          texto: 'Cost High to Low',
+                          isActive: costHighToLow,
+                          function: () {
+                            setState(() => costHighToLow = !costHighToLow);
+                          },
+                        ),
+                        ListTileCheck(
+                          texto: 'Cost Low to High',
+                          isActive: costLowToHigh,
+                          function: () {
+                            setState(() => costLowToHigh = !costLowToHigh);
+                          },
+                        ),
+                        ListTileCheck(
+                          texto: 'Most Popular',
+                          isActive: mostPopular,
+                          function: () {
+                            setState(() => mostPopular = !mostPopular);
+                          },
+                        ),
+                      ],
+                    )),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(top: 15.0, bottom: 5.0, left: 15.0),
+                  child: headerText(
+                      texto: 'FILTER',
+                      color: gris,
+                      fontWeight: FontWeight.w600,
+                      fontsize: 17.0
+                  ),
+                ),
+                Container(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Column(
+                      children: [
+                        ListTileCheck(
+                          texto: 'Open Now',
+                          isActive: openNow,
+                          function: () {
+                            setState(() => openNow = !openNow);
+                          },
+                        ),
+                        ListTileCheck(
+                          texto: 'Credit Cards',
+                          isActive: creditCards,
+                          function: () {
+                            setState(() => creditCards = !creditCards);
+                          },
+                        ),
+                        ListTileCheck(
+                          texto: 'Alcohol Served',
+                          isActive: alcoholServed,
+                          function: () {
+                            setState(() => alcoholServed = !alcoholServed);
+                          },
+                        ),
+                      ],
+                    )),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(top: 15.0, bottom: 5.0, left: 15.0),
+                  child: headerText(
+                      texto: 'PRICE',
+                      color: gris,
+                      fontWeight: FontWeight.w600,
+                      fontsize: 17.0
+                  ),
+                ),
+                PriceFilter()
+              ]
               )
           )
         ],
@@ -52,6 +152,7 @@ class _FilterPageState extends State<FilterPage> {
     );
   }
 }
+
 
 Widget _appBar(BuildContext context) {
   return AppBar(
