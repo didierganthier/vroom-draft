97d600d6c3a1a4de6f125b61585ef8416def98c6
Woodens <woodgregory15@gmail.com>
2021-07-20 13:49:39 -0400

    place page

--- lib/src/features/presentation/place_detail_page/View/place_detail_page.dart
+++ lib/src/features/presentation/place_detail_page/View/place_detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vroom/src/colors/colors.dart';
 import 'package:vroom/src/features/presentation/widgets/Headers/header_text.dart';
+import 'package:vroom/src/features/presentation/widgets/backButtons/back_button.dart';
 
 class PlaceDetailPage extends StatelessWidget {
   @override
@@ -26,12 +27,34 @@ class PlaceDetailPage extends StatelessWidget {
                   ),
                   Wrap(
                     children: [
-                      _promoButton(), _infoPlace()
+                      _promoButton(), _infoPlace(), _infoPlaceStats(), _offerBanner()
                     ],
                   )
                 ],
               ),
             ),
+            leading: Builder(builder: (BuildContext context){
+              return backButton(context, Colors.white);
+            }),
+            actions: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Image(
+                  color: Colors.white,
+                  width: 28,
+                  height: 28,
+                  image: AssetImage('assets/shareB.png'),
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.bookmark_border,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                onPressed: () {},
+              )
+            ],
           )
         ],
       ),
@@ -86,5 +109,114 @@ Widget _infoPlace() {
         ),
       ),
     ],
+  );
+}
+
+Widget _infoPlaceStats() {
+  return Container(
+    margin: EdgeInsets.only(top: 26.0),
+    padding: EdgeInsets.symmetric(horizontal: 40.0),
+    height: 70.0,
+    decoration: BoxDecoration(
+      border: Border(
+        top: BorderSide(color: Colors.white),
+        bottom: BorderSide(color: Colors.white)
+      )
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.star,
+                  color: Colors.white,
+                  size: 19.0,
+                ),
+                headerText(texto: '4.5', color: Colors.white, fontWeight: FontWeight.bold, fontsize: 15.0)
+              ],
+            ),
+            headerText(texto: "351 Ratings", color: gris, fontWeight: FontWeight.w500, fontsize: 15.0),
+          ],
+        ),
+        Container(
+          height: 40.0,
+          decoration: BoxDecoration(
+            border: Border(right: BorderSide(color: Colors.white))
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.bookmark,
+                  color: Colors.white,
+                  size: 19.0,
+                ),
+                headerText(texto: '137K', color: Colors.white, fontWeight: FontWeight.bold, fontsize: 15.0)
+              ],
+            ),
+            headerText(texto: "Favorites", color: gris, fontWeight: FontWeight.w500, fontsize: 15.0),
+          ],
+        ),
+        Container(
+          height: 40.0,
+          decoration: BoxDecoration(
+              border: Border(right: BorderSide(color: Colors.white))
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.photo,
+                  color: Colors.white,
+                  size: 19.0,
+                ),
+                headerText(texto: '346', color: Colors.white, fontWeight: FontWeight.bold, fontsize: 15.0)
+              ],
+            ),
+            headerText(texto: "Photo", color: gris, fontWeight: FontWeight.w500, fontsize: 15.0),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _offerBanner() {
+  return Container(
+    color: Color.fromRGBO(255, 237, 214, 1.0),
+    padding: EdgeInsets.all(20),
+    height: 90.0,
+    child: Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            headerText(texto: 'New! Try Pickup', color: orange, fontWeight: FontWeight.bold,fontsize: 15.0),
+            headerText(texto: 'Pickup on your time. Your order is  \n ready when you are', color: primaryColor, fontWeight: FontWeight.w400,fontsize: 13.0),
+          ],
+        ),
+        Spacer(),
+        RaisedButton(
+          elevation: 0.5,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          color: orange,
+          onPressed: () {},
+          child: headerText(texto: 'Order now', color: Colors.white, fontWeight: FontWeight.bold, fontsize: 13.0)
+        )
+      ],
+    ),
   );
 }
